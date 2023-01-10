5c5a64d392ed24347e0157ec0ca574d2213cb179
shailesh gontewar <shaileshsgontewar@gmail.com>
2023-01-10 17:44:16 +0530

    api inserted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./component/Home";
 import Sidebar from "./component/sidebar";
-import Dataprovider from "./context/GlobalStore";
+// import Dataprovider from "./context/GlobalStore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./component/Dashboard";
 import Appoinment from "./component/Appinment";
@@ -11,18 +11,17 @@ import Password from "./component/password";
 function App() {
   return (
     <div className="main-container">
-      <Dataprovider>
-        <BrowserRouter>
-          <Sidebar />
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/dashboard" element={<Dashboard />}></Route>
-            <Route path="/appoinment" element={<Appoinment />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/password" element={<Password />}></Route>
-          </Routes>
-        </BrowserRouter>
-      </Dataprovider>
+      {/* <Dataprovider></Dataprovider> */}
+      <BrowserRouter>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/appoinment" element={<Appoinment />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/password" element={<Password />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/pages/Post.js
+++ src/component/pages/Post.js
@@ -1,37 +1,90 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { AiFillStar, AiFillHome } from "react-icons/ai";
-import { Data } from "../../context/GlobalStore";
+// import { Data } from "../../context/GlobalStore";
+import axios from "axios";
+const client = axios.create({
+  baseURL: "https://api.soowgood.com/api/Searches/Provider",
+});
 
 const Post = () => {
-  const [data] = useContext(Data);
+  // const [data] = useContext(Data);
+  const [post, setPost] = useState([]);
+  console.log(post);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const fetchData = async () => {
+      try {
+        const response = await client.post(
+          "/",
+
+          {
+            UserRole: "Provider",
+          },
+          { signal }
+        );
+        setPost(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+    return () => {
+      console.log("data unmounted....");
+      controller.abort();
+    };
+  }, []);
+
   return (
     <>
       <div className="container">
         <div className="post-container">
-          {data.map((post) => {
+          {post.map((post) => {
             return (
-              <div className="post" key={post.id}>
+              <div className="post" key={post.providerId}>
                 <div className="info">
-                  <img src={post.image} alt="d" />
+                  <img
+                    src={
+                      post.providerImage
+                        ? post.providerImage
+                        : "./img/img11.jpg"
+                    }
+                    alt="doct"
+                  />
                   <h3>{post.name}</h3>
-                  <p>Not Available</p>
+                  <p>
+                    {post.service < 10
+                      ? post.service
+                        ? post.service
+                        : "Not Available"
+                      : post.service.slice(0, 15)}
+                  </p>
                   <div className="review">
                     <AiFillStar className="star" />
                     <AiFillStar className="star" />
                     <AiFillStar className="star" />
+                    <sub>{post.totalreview} review</sub>
                   </div>
                 </div>
                 <div className="address">
-                  <h3>
+                  <h4>
                     <span>
                       <AiFillHome className="add-icon" />
                     </span>
                     Place
-                  </h3>
-                  <b>{post.clinicname}</b>
-                  <p>{post.address}</p>
+                  </h4>
+                  <b>
+                    {post.clinicName < 15
+                      ? post.clinicName
+                      : post.clinicName.slice(0, 16)}
+                  </b>
+                  <p>
+                    {post.clinicAddress < 20
+                      ? post.clinicAddress
+                      : `${post.clinicAddress.slice(0, 50)}...`}
+                  </p>
                 </div>
-                <button className="online-btn">Online</button>
+                <button className="online-btn">{post.appointmentType}</button>
                 <div className="btn-container">
                   <button className="view-btn">View more</button>
                   <button className="book-btn">Book now</button>
